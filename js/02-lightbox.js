2cdff94ff37f058306fea0fe6f13942c3567fce6
Mara-ya <97354812+Mara-ya@users.noreply.github.com>
2022-06-07 10:58:30 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,11 +10,9 @@ function createImagesMarkup(galleryItems) {
     return galleryItems
     .map(({preview, original, description}) => {
         return `
-        <li>
         <a class="gallery__item" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" />
         </a>
-        </li>
         `
     })
     .join('');  
